3b2866430421716b0cf87479a2094d2e7f6fb1a2
Bader Gharib <badergharib87@gmail.com>
2025-02-18 01:07:05 +0200

    make the feature's design

--- src/components/WeatherNow.jsx
+++ src/components/WeatherNow.jsx
@@ -1,23 +1,25 @@
+import React from "react";
+import storm from "../../public/images/weather-icons/storm.svg";
+
 export default function WeatherNow() {
   return (
     <>
       <div className="container">
         <div className="weather-now-section">
           <div className="weather-status-icon">
-            <img src="https://placehold.co/600x400" alt="Placeholder" />
-            <div className="details">
-              <p>
-                <span>Temperature</span>
-                <span>10 to 11 C</span>
-              </p>
-              <p>
-                <span>Humidity</span> <span>78%</span>
-                <span>Pressure</span> <span>1008.48</span>
-              </p>
-            </div>
+            <img src={storm} alt="Placeholder" />
+          </div>
+          <div className="details">
+            <p className="temperature">
+              <span>Temperature 10 to 11 C</span>
+            </p>
+            <p className="atmosphere">
+              <span>Humidity 78%</span>
+              <span>Pressure 1008.48</span>
+            </p>
           </div>
         </div>
-        <div className="weather-over-24h-section"></div>
+        {/* <div className="weather-over-24h-section"></div> */}
       </div>
     </>
   );
