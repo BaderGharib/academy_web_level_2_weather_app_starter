7b137ab1510d7c221b40632807250a698aa8a244
Bader Gharib <badergharib87@gmail.com>
2025-02-17 21:56:56 +0200

    fixed search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,8 @@
 export default function Search() {
   return (
-    <div className="container">
-      <div className="header">
-        <input type="text" placeholder="Type in a city name" />
-        <button>See Weather</button>
-      </div>
+    <div className="">
+      <input type="text" className="search-bar" />
+      <button>See Weather</button>
     </div>
   );
 }
