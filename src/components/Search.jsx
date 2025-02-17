fc40cd3ba3c01e00b3c85a478cd5461c7496f0e6
Bader Gharib <badergharib87@gmail.com>
2025-02-17 21:37:51 +0200

    finish search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <div className="container">
+      <div className="header">
+        <input type="text" placeholder="Type in a city name" />
+        <button>See Weather</button>
+      </div>
+    </div>
+  );
+}
